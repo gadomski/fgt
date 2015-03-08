a93559726ea7833d01b9107f385a43e87d65ab50
Pete Gadomski <pete.gadomski@gmail.com>
2015-03-08 18:27:20 -0500

    Be a bit clearer with the direct tests

--- test/direct_test.cpp
+++ test/direct_test.cpp
@@ -9,19 +9,29 @@
 namespace fgt {
 
 
-TEST(Direct, ReferenceImplementation) {
-    arma::mat source, target;
-    source.load(test_data_path("X.csv"));
-    target.load(test_data_path("Y.csv"));
-    source = source.rows(0, 99);
-    target = target.rows(0, 99);
-    double bandwidth = 0.4;
+TEST(Direct, OneTargetOneSource) {
+    arma::mat source = {0, 0};
+    source.reshape(1, 2);
+    arma::mat target = {1, 2};
+    target.reshape(1, 2);
 
-    Direct direct(source, bandwidth);
+    Direct direct(source, 1);
     arma::vec g = direct.compute(target);
+    EXPECT_EQ(g.n_rows, 1);
+    EXPECT_DOUBLE_EQ(0.006737946999085467, g(0));
+}
 
-    EXPECT_EQ(g.n_rows, 100);
-    EXPECT_DOUBLE_EQ(4.3701728529168333, arma::min(g));
-    EXPECT_DOUBLE_EQ(93.72376652714631, arma::max(g));
+
+TEST(Direct, TwoTargetsTwoSources) {
+    arma::mat source = {0, 0, 0, 1};
+    source.reshape(2, 2);
+    arma::mat target = {0, 0.5, 0.25, 0.25};
+    target.reshape(2, 2);
+
+    Direct direct(source, 0.2);
+    arma::vec g = direct.compute(target);
+    EXPECT_EQ(g.n_rows, 2);
+    EXPECT_DOUBLE_EQ(0.2096121683000387, g(0));
+    EXPECT_DOUBLE_EQ(0.00040464667729846903, g(1));
 }
 }
