f25d852f7eae2feb1818bd7382de207c8e052186
Pete Gadomski <pete.gadomski@gmail.com>
2015-03-08 11:59:11 -0500

    Shrink matrices for direct
    Because slow

--- test/direct_test.cpp
+++ test/direct_test.cpp
@@ -13,12 +13,14 @@ TEST(Direct, ReferenceImplementation) {
     arma::mat X, Y;
     X.load(test_data_path("X.csv"));
     Y.load(test_data_path("Y.csv"));
+    X = X.rows(0, 99);
+    Y = Y.rows(0, 99);
     double h = 0.4;
 
     arma::vec G = direct(X, Y, h);
 
-    EXPECT_EQ(G.n_rows, 5000);
-    EXPECT_DOUBLE_EQ(359.47775885562908, arma::min(G));
-    EXPECT_DOUBLE_EQ(2221.3622088016318, arma::max(G));
+    EXPECT_EQ(G.n_rows, 100);
+    EXPECT_DOUBLE_EQ(4.3701728529168333, arma::min(G));
+    EXPECT_DOUBLE_EQ(93.72376652714631, arma::max(G));
 }
 }
