f6d515fabbde7d7412614f0cd8fafd477fec9bb2
Pete Gadomski <pete.gadomski@gmail.com>
2015-03-12 09:34:47 -0500

    Refactor target configuration to cmake function

---
+++ cmake/configure_target.cmake
@@ -0,0 +1,22 @@
+function(fgt_configure_target target)
+    set(one_value_args NAME EXTRA_COMPILE_FLAGS)
+    cmake_parse_arguments(TARGET "" "${one_value_args}" "" ${ARGN})
+    set_property(TARGET ${target} APPEND_STRING PROPERTY
+        COMPILE_FLAGS " -std=c++11 -Wall -pedantic -Werror -Wno-nested-anon-types ${TARGET_EXTRA_COMPILE_FLAGS}")
+
+    set_target_properties(${target} PROPERTIES
+        VERSION ${FGT_VERSION}
+        SOVERSION ${FGT_SOVERSION}
+        )
+
+    if (TARGET_NAME)
+        set_property(TARGET ${target} PROPERTY OUTPUT_NAME ${TARGET_NAME})
+    endif()
+
+    if(ARMA_64BIT_WORD)
+        target_compile_definitions(${target} PUBLIC ARMA_64BIT_WORD)
+    endif()
+    if(ARMA_NO_DEBUG)
+        target_compile_definitions(${target} PUBLIC ARMA_NO_DEBUG)
+    endif()
+endfunction()
