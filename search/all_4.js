9e73ef14a7d430e92c539f538ae782fade41da98
Travis CI <travis@localhost>
2016-02-19 18:01:46 +0000

    scripts/gh-pages.sh

--- search/all_4.js
+++ search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['ifgt',['Ifgt',['../classfgt_1_1_ifgt.html',1,'fgt']]],
-  ['ifgt',['Ifgt',['../classfgt_1_1_ifgt.html#ad523442e6c62ad42102e5338b70f89f3',1,'fgt::Ifgt']]]
+  ['ifgt',['Ifgt',['../classfgt_1_1_ifgt.html#ad523442e6c62ad42102e5338b70f89f3',1,'fgt::Ifgt::Ifgt(const arma::mat &amp;source, double bandwidth, double epsilon)'],['../classfgt_1_1_ifgt.html#a227e596caa40861085fe1bd78b888fb3',1,'fgt::Ifgt::Ifgt(const arma::mat &amp;source, double bandwidth, double epsilon, bool data_adaptive)']]]
 ];
