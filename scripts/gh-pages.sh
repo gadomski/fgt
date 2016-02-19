ea886deb02784eecdb5a456ff2cce86a8a5ca2e0
Pete Gadomski <pete.gadomski@gmail.com>
2016-02-19 08:28:24 -0700

    More gh-pages tweaks
    Only build on first job, and set the username and email.

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -24,6 +24,7 @@ abort() {
 if [ "${TRAVIS}" = "true" ]; then
     [ "${TRAVIS_PULL_REQUEST}" = "false" ] || skip "Not building docs for pull requests"
     [ "${TRAVIS_BRANCH}" = "master" ] || skip "Only building docs for master branch"
+    [ "${TRAVIS_JOB_NUMBER}" = "${TRAVIS_BUILD_NUMBER}.1" ] || skip "Only build docs once"
 
     sudo apt-get install -y doxygen
 fi
@@ -42,6 +43,8 @@ git add --all
 git diff-index --quiet HEAD || git commit -m "scripts/gh-pages.sh"
 
 if [ "${TRAVIS}" = "true" ]; then
+    git config user.name "Travis CI"
+    git config user.email "travis@localhost"
     git push "https://${GH_TOKEN}@github.com/gadomski/fgt.git" gh-pages
 else
     git push git@github.com:gadomski/fgt gh-pages
