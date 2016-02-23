52f4a2f949c45724d3f28c5bf2c8964c2948dfd3
Pete Gadomski <pete.gadomski@gmail.com>
2016-02-23 08:14:36 -0700

    Remove VERSION.txt
    We don't use it any more.

--- test/version_test.cpp
+++ test/version_test.cpp
@@ -1,16 +1,8 @@
-#include <fstream>
-
 #include "gtest/gtest.h"
 
 #include "test/support.hpp"
 
 namespace fgt {
 
-TEST(Version, MatchesVersionFile) {
-    std::string v = version();
-    std::ifstream file(project_source_filename("VERSION.txt"));
-    std::string version_txt;
-    std::getline(file, version_txt);
-    EXPECT_EQ(version_txt, v);
-}
+TEST(Version, Compiles) { std::string v = version(); }
 }
