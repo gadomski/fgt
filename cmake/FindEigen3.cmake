9f4c0c2cb31da4dc25b8d8747b09dfcb30c817f1
hochshi <hochshi@users.noreply.github.com>
2023-02-20 15:50:36 +0200

    Trying to make fgt work nicely with my setup.

--- cmake/FindEigen3.cmake
+++ cmake/FindEigen3.cmake
@@ -15,8 +15,6 @@
 # Copyright (c) 2009 Benoit Jacob <jacob.benoit.1@gmail.com>
 # Redistribution and use is allowed according to the terms of the 2-clause BSD license.
 
-if${CMAKE_VERSION} VERSION_LESS "3.1.0")
-
 if(NOT Eigen3_FIND_VERSION)
   if(NOT Eigen3_FIND_VERSION_MAJOR)
     set(Eigen3_FIND_VERSION_MAJOR 2)
@@ -80,5 +78,3 @@ else (EIGEN3_INCLUDE_DIR)
   mark_as_advanced(EIGEN3_INCLUDE_DIR)
 
 endif(EIGEN3_INCLUDE_DIR)
-
-endif()
