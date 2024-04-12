4f9621624e42ac5cbfd6a83ff248f6a15e62ce41
Howard Butler <hobu.inc@gmail.com>
2024-04-12 14:29:22 -0500

    no BUILD_SHARED_LIBS on windows, use STATIC library for windows output

--- scripts/cmake/windows-latest-cmake.sh
+++ scripts/cmake/windows-latest-cmake.sh
@@ -9,5 +9,4 @@ cmake .. \
     -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
     -DWITH_TESTS=ON \
     -DCMAKE_BUILD_TYPE=Release \
-    -Dgtest_force_shared_crt=ON \
-    -DBUILD_SHARED_LIBS=ON
+    -Dgtest_force_shared_crt=ON
