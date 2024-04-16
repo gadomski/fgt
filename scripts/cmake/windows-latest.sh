a989afda61838ad66558fdcbc0432d45a3e010c8
Howard Butler <hobu.inc@gmail.com>
2024-04-16 13:11:22 -0500

    Get CI healthy on all three platforms ✅  (#54)
    * use micromamba builder
    
    * use standard CMake find_package CONFIG for Eigen
    
    * openmp for linux only
    
    * drop environment cache
    
    * parameterize environments
    
    * use parameterized environments
    
    * setup msvc
    
    * use msvc
    
    * gtest_force_shared_crt=ON
    
    * use OBJECT library type for Library-C++
    
    * SHARED instead of DYNAMIC
    
    * more windows WIP
    
    * == is not equality in bash :shrug:
    
    * parametrize cmake invocations
    
    * correct paths
    
    * working-directory
    
    * point to scripts
    
    * more test lint
    
    * parameterize test scripts
    
    * point to gtest.dll when running tests
    
    * no BUILD_SHARED_LIBS on windows, use STATIC library for windows output
    
    * refactor test script locations
    
    * fix up GHA config
    
    * trigger CI

---
+++ scripts/cmake/windows-latest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pwd
+where cl.exe
+export CC=cl.exe
+export CXX=cl.exe
+cmake .. \
+    -G Ninja \
+    -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
+    -DWITH_TESTS=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -Dgtest_force_shared_crt=ON
